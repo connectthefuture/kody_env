0fb8d7bdcb9f2ce6534e79a9ce0852c44b73cb51
jh3y <jhey.tompkins@gmail.com>
2015-06-22 14:03:15 +0100

    Add project utility for importing personal projects and setup

---  config.link/fish/functions/open_project.fish
+++
@@ -1,4 +0,0 @@
-function open_project --description 'Open a workspace project in atom'
-  cd ~/Documents/Workspace/$argv
-  atom .
-end
---  config.link/fish/functions/project_utils.fish
+++  config.link/fish/functions/project_utils.fish
@@ -14,3 +14,33 @@ function gen_gulp_project --description 'generate gulp project'
   git init
   git add --all
 end
+
+
+function open_project --description 'Open a workspace project in atom'
+  cd ~/Documents/Workspace/$argv
+  atom .
+end
+
+
+function setup_project --description 'Set up project by running npm, gulp etc.'
+  cd $argv
+  if test -e package.json
+    npm install
+  end
+  if test -e bower.json
+    bower install
+  end
+  atom .
+  if test -e gulpfile.js
+    gulp
+  end
+end
+
+function import_project --description 'Import a project and install deps'
+    git clone https://github.com/jh3y/$argv[1]
+    if test (count $argv) -gt 1
+      if [ $argv[2] = "true" ]
+        setup_project $argv[1]
+      end
+    end
+end
